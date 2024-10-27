0cca6eb24ceac41189f27bd174712efe1c517449
Henrique Costa <henriquecosta@MacBook-Air-de-Henrique.local>
2024-10-27 13:05:18 -0300

    fix: adicionei parametro color no tabBarIcon puxando do screenOptions

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -24,27 +24,28 @@ const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>()
 export function AppRoutes() {
   const { tokens } = gluestackUIConfig;
   const iconSize = tokens.space["6"];
-  const iconColor = "black"
   
   return (
     <Navigator screenOptions={{
       headerShown: false,
-      tabBarShowLabel: false
+      tabBarShowLabel: false,
+      tabBarActiveTintColor: tokens.colors.green500,
+      tabBarInactiveTintColor: tokens.colors.gray200,
     }}>
       <Screen
         name='home'
         component={Home}
-        options={{ tabBarIcon: () => <HomeSvg fill={ iconColor } width={iconSize} height={iconSize} /> }}
+        options={{ tabBarIcon: ({ color }) => <HomeSvg fill={ color } width={iconSize} height={iconSize} /> }}
       />
       <Screen
         name='history'
         component={History}
-        options={{ tabBarIcon: () => <HistorySvg fill={ iconColor } width={iconSize} height={iconSize} /> }}
+        options={{ tabBarIcon: ({ color }) => <HistorySvg fill={ color } width={iconSize} height={iconSize} /> }}
       />
       <Screen
         name='profile'
         component={Profile}
-        options={{ tabBarIcon: () => <ProfileSvg fill={ iconColor } width={iconSize} height={iconSize} /> }}
+        options={{ tabBarIcon: ({ color }) => <ProfileSvg fill={ color } width={iconSize} height={iconSize} /> }}
       />
       <Screen
         name='exercise'
