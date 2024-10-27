0a15bd36e9abcbd1d45c465d1994a6e0236c788b
Henrique Costa <henriquecosta@MacBook-Air-de-Henrique.local>
2024-10-27 10:24:08 -0300

    style: telas de exercise, history, home, profile, foram adicionadas

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import { StatusBar } from 'react-native';
 import { useFonts, Roboto_700Bold, Roboto_400Regular } from '@expo-google-fonts/roboto';
 
 import { GluestackUIProvider, Text, Center } from '@gluestack-ui/themed';
-import { config } from './config/gluestack-ui.config';
+import { config } from './src/config/gluestack-ui.config';
 import { Loading } from '@components/Loading';
 import { Routes } from '@routes/index';
 
---
+++ src/screens/Home.tsx
@@ -0,0 +1,10 @@
+import { Center, Text } from "@gluestack-ui/themed";
+
+
+export function Home() {
+    return (
+        <Center flex={1}>
+            <Text>Home</Text>
+        </Center>
+    )
+}
