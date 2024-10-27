ee432d650f81eabf82f0b9aedd346081aab9c66d
Henrique Costa <henriquecosta@MacBook-Air-de-Henrique.local>
2024-10-27 17:07:07 -0300

    feat: component group adicionado na home

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,3 +1,4 @@
+import { Group } from "@components/Group";
 import { HomeHeader } from "@components/HomeHeader";
 import { VStack, Text } from "@gluestack-ui/themed";
 
@@ -6,6 +7,7 @@ export function Home() {
     return (
         <VStack flex={1}>
             <HomeHeader />
+            <Group name="teste" isActive={false} />
         </VStack>
     )
 }
