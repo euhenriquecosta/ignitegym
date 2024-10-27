8307dbf1c49b0258537ab5f72cf5d03ade6cf6dd
Henrique Costa <henriquecosta@MacBook-Air-de-Henrique.local>
2024-10-27 17:27:54 -0300

    style: estilizando header de exercicios

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -3,7 +3,7 @@ import { FlatList } from "react-native";
 
 import { Group } from "@components/Group";
 import { HomeHeader } from "@components/HomeHeader";
-import { VStack, Text, HStack } from "@gluestack-ui/themed";
+import { VStack, Text, HStack, Heading } from "@gluestack-ui/themed";
 
 
 export function Home() {
@@ -28,13 +28,26 @@ export function Home() {
         )}
         horizontal
         showsHorizontalScrollIndicator={false}
-        contentContainerStyle={{ paddingHorizontal: 32 }}
+        contentContainerStyle={{ 
+          paddingHorizontal: 32
+         }}
         style={{
           marginVertical: 40,
           maxHeight: 44,
           minHeight: 44
         }}
       />
+      <VStack px="$8">
+        <HStack justifyContent="space-between" mb="$5" alignItems="center">
+          <Heading color="$gray200" fontSize="$md" fontFamily="$heading">
+            Exercicios
+          </Heading>
+
+          <Text color="$gray200" fontSize="$sm" fontFamily="$body">
+            4
+          </Text>
+        </HStack>
+      </VStack>
     </VStack>
   )
 }
