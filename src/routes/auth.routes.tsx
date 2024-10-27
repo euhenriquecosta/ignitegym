0475c9758b4a01a105d559c51f17b7f0870158a6
Henrique Costa <henriquecosta@MacBook-Air-de-Henrique.local>
2024-10-27 01:08:47 -0300

    feat: tipagem das rotas

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -1,7 +1,14 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { createNativeStackNavigator, NativeStackNavigationProp} from '@react-navigation/native-stack'
 
 import { SignIn } from '@screens/SignIn';
 import { SignUp } from '@screens/SignUp';
+
+type AuthRoutes = {
+  signIn: undefined;
+  signUp: undefined;
+}
+
+export type AuthNavigatorRoutesProps = NativeStackNavigationProp<AuthRoutes>;
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
