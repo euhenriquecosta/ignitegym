846f5b1b97958d099fb9a67decf9141b174a9843
Henrique Costa <henriquecosta@MacBook-Air-de-Henrique.local>
2024-10-26 23:52:54 -0300

    feat: ativar borda quando o botao e selecionado

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,7 +1,9 @@
-import { Center, Image, Text, VStack } from "@gluestack-ui/themed"
+import { Center, Heading, Image, Text, VStack } from "@gluestack-ui/themed"
 
 import BackgroundImg from "@assets/background.png"
 import Logo from "@assets/logo.svg"
+
+import { Input } from "@components/Input"
 
 export function SignIn() {
   return (
@@ -14,14 +16,22 @@ export function SignIn() {
         alt="Pessoas treinando"
         position="absolute"
       />
-      
-      <Center my="$24">
-        <Logo />
+      <VStack flex={1} px="$10" pb="$16">
+        <Center my="$24">
+          <Logo />
 
-        <Text color="$gray100" fontSize="$sm">
-          Treine sua mente e seu corpo.
-        </Text>
-      </Center>
+          <Text color="$gray100" fontSize="$sm">
+            Treine sua mente e seu corpo.
+          </Text>
+        </Center>
+
+        <Center gap="$2">
+          <Heading color="$gray100" fontSize="$3xl">Acesse a conta</Heading>
+
+          <Input placeholder="E-mail"/>
+          <Input placeholder="Senha"/>
+        </Center>
+      </VStack>
 
     </VStack>
   )
