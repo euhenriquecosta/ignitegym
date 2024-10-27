07f7c1ba77bbb4ec9a18e2973d39ee49f8e649b2
Henrique Costa <henriquecosta@MacBook-Air-de-Henrique.local>
2024-10-27 14:55:31 -0300

    feat: component Home Header adicionado

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,11 @@
+import { HomeHeader } from "@components/HomeHeader";
 import { Center, Text } from "@gluestack-ui/themed";
 
 
 export function Home() {
     return (
         <Center flex={1}>
-            <Text color="$white">Home</Text>
+            <HomeHeader />
         </Center>
     )
 }
