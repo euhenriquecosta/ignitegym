aa38f66ba974fbb2316742844290b4d0f49c12ab
Henrique Costa <henriquecosta@MacBook-Air-de-Henrique.local>
2024-10-26 23:21:42 -0300

    style: adicionando imagem de background na tela de signin

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { useFonts, Roboto_700Bold, Roboto_400Regular } from '@expo-google-fonts/
 import { GluestackUIProvider, Text, Center } from '@gluestack-ui/themed';
 import { config } from './config/gluestack-ui.config';
 import { Loading } from '@components/Loading';
+import { SignIn } from '@screens/SignIn';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular });
@@ -13,10 +14,8 @@ export default function App() {
 
       <StatusBar backgroundColor="transparent" barStyle="light-content" translucent />
 
-      {!fontsLoaded ? (
-        <Center flex={1} bg="$info600">
-          <Text>Home</Text>
-        </Center>
+      {fontsLoaded ? (
+        <SignIn />
       ) : (
         <Loading />
       )}
